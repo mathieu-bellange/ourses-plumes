4d35f8748063158765bd6974642c4887aecdd170
mathieu-bellange <mbellange@gmail.com>
2013-10-15 22:44:28 +0200

    modif sur le pom web

--- web-resources/src/main/js/editor.js
+++ web-resources/src/main/js/editor.js
@@ -1,10 +1,32 @@
-
 function  sendArticle(){
-	var title = $('#title').val();
-	var data = $('#editor').val();
-	var publication = $('#publication').val();
+	var data = {
+			title : $('#title').val(),
+			body : $('#editor').val(),
+			publication : $('#publication').val(),
+			label : $('#label').val(),
+			pseudo : $('#pseudo').val(),
+			categorie : $('#categorie').val()
+	};
+	
+//	$.post( "editor.html",data, function(status) {
+//		 alert(status);
+//		});
+	$.ajax({
+		  type: "POST",
+		  url: "http://localhost:8080/rest/article",
+		  contentType: "application/json; charset=utf-8",
+		  data : JSON.stringify(data),
+		  success: function(jqXHR, status, errorThrown) {
+				 alert(status);
+			},
+		  error: function(jqXHR, status, errorThrown) {
+				 alert(status);
+			},
+		  dataType: "json"
+		});
 	return true;
 };
+
 
 $(document).ready(function(){
 	$('#editor').ckeditor();
