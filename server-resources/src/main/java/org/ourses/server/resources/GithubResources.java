e04ff036c3f4ad754185d914c7b308c42b929d1e
mathieu-bellange <mbellange@gmail.com>
2014-03-13 16:47:58 +0100

    mise en place urlrewriter ajout page d'ajout de bug github

---
+++ server-resources/src/main/java/org/ourses/server/resources/GithubResources.java
@@ -0,0 +1,29 @@
+package org.ourses.server.resources;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.ourses.server.external.GithubBug;
+import org.ourses.server.external.GithubRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Path("/github")
+public class GithubResources {
+
+    @Autowired
+    private GithubRequest githubService;
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    // TODO activé securité @RequiresRoles(value = { RolesUtil.ADMINISTRATRICE })
+    public Response addBug(GithubBug githubBug) {
+        githubService.addIssue(githubBug);
+        return Response.ok(githubBug).build();
+    }
+
+}
