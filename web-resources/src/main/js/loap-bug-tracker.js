0ec712687b453436193ae022f7b8530b69aae357
lny <lny.mrt@gmail.com>
2014-03-20 19:46:51 +0100

    Mise à jour bug-tracker
    HTML : reordering, required attr, temp styles
    JS : indentation (tabs to whitespaces)

--- web-resources/src/main/js/loap-bug-tracker.js
+++ web-resources/src/main/js/loap-bug-tracker.js
@@ -1,27 +1,27 @@
 /* Domain */
 
 function GithubBug(title,body) {
-	this.title = title;
-	this.body = body;
-	this.json = function() {
-		return JSON.stringify(this);
-	};
+  this.title = title;
+  this.body = body;
+  this.json = function() {
+    return JSON.stringify(this);
+  };
 }
 
 /* Events */
 $( "#new-bug" ).submit(function( event ) {
-	var bug = new GithubBug($("#bug-title").val(),$("#bug-body").val());
-	$.ajax({
+  var bug = new GithubBug($("#bug-title").val(),$("#bug-body").val());
+  $.ajax({
         type: "POST",
         url: "/rest/github",
         data: bug.json(),
         contentType : "application/json; charset=utf-8",
-        success: function (data) {    
-        	alert("ok");
+        success: function (data) {
+          alert("ok");
         },
         error: function(jqXHR, text, error){
-      	  alert("error");
+          alert("error");
         },
         dataType : "json"
-	  });
+    });
 });
