3b74f362464f2c27ccb6dae85f34f0a942418c2b
lny <lny.mrt@gmail.com>
2014-03-20 22:23:36 +0100

    Mise à jour fichiers JS
    - Ajouté loap-conf.js (global vars)
    - Renommé loap.js en loap-core.js

---
+++ web-resources/src/main/js/loap-conf.js
@@ -0,0 +1,15 @@
+﻿/*
+ * VERSION
+ * Les Ourses à plumes
+ * Javascript Configuration
+ * Required jQuery Library
+ * ver. alpha 2
+ *
+ */
+ 
+/* ------------------------------------------------------------------ */
+/* # Global variables initialization */
+/* ------------------------------------------------------------------ */
+
+var loading_feedback = false; // Show js file loaded alerts on document ready. Default = false
+var dev_toolbar      = true;  // Show dev toolbar. Default = false
