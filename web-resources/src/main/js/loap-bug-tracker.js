97d7af11d19adacac675c58bf66184c6f624c19d
mathieu-bellange <mbellange@gmail.com>
2014-03-21 18:13:32 +0100

    modification sur reférence foundation ajout validation sur form bug-tracking

--- web-resources/src/main/js/loap-bug-tracker.js
+++ web-resources/src/main/js/loap-bug-tracker.js
@@ -9,18 +9,21 @@ function GithubBug(title,body) {
 
 /* Events */
 $( "#new-bug" ).submit(function( event ) {
-	var bug = new GithubBug($("#bug-title").val(),$("#bug-body").val());
-	$.ajax({
-				type: "POST",
-				url: "/rest/github",
-				data: bug.json(),
-				contentType : "application/json; charset=utf-8",
-				success: function (data) {
-					alert("ok");
-				},
-				error: function(jqXHR, text, error){
-					alert("error");
-				},
-				dataType : "json"
+	if ($( "#new-bug" ).checkValidity()){
+		var bug = new GithubBug($("#bug-title").val(),$("#bug-body").val());
+		$.ajax({
+			type: "POST",
+			url: "/rest/github",
+			data: bug.json(),
+			contentType : "application/json; charset=utf-8",
+			success: function (data) {
+				alert("ok");
+			},
+			error: function(jqXHR, text, error){
+				alert("error");
+			},
+			dataType : "json"
 		});
+	}
+	
 });
