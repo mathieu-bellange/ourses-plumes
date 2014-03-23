a5e026a8eff7e9e1242ce039a6521819a63b2a7f
julie-petit <julie.marie1403@gmail.com>
2014-03-23 18:13:24 +0100

    liste des comptes - alerts
    gestion des erreurs dans la liste des comptes et ajout du bouton
    nouvelle contributrice

--- web-resources/src/main/js/loap-accounts.js
+++ web-resources/src/main/js/loap-accounts.js
@@ -12,46 +12,63 @@ function OurseAuthzInfo(id,role){
 $.getJSON("/rest/role", function(rolesBdd) {
 	roles = rolesBdd;
 	getAccount();
+}).fail(function(){
+	$("#comptes-alert").remove("success");
+	$("#comptes-alert").addClass("warning");
+	$(".comptes-alert-message").text("Une erreur technique s'est produite, prévenez l'administateur du site");
+	$("#comptes-alert").fadeIn(500);
 });
 
 function getAccount(){
-	$.getJSON("/rest/account", function(accounts) {
-		$.each(accounts, function(index, value) {
-			//création de la ligne tr
-			var ligne = $("<tr>").attr("data-account-id",value.id);
-			//création des colonnes
-			var pseudoCol = $("<td>");
-			var mailCol = $("<td>");
-			var roleCol = $("<td>");
-			var delCol = $("<td>");
-			//création et config des composants dans les colonnes
-			var pseudo = $("<label>").text(value.profile.pseudo);
-			var mail = $("<label>").text(value.mail);
-			var comboRole = $("<select>");
-			comboRole.on("change", value, updateEvent);
-			// recup la liste des roles et creer les options
-			var options = comboRole.prop('options');
-			$.each(roles, function(val, text) {
-				options[options.length] = new Option(text.role, text.id);
+	$.ajax({
+		type : "GET",
+		url : "/rest/account",
+		contentType : "application/json; charset=utf-8",
+		success : function(accounts, status, jqxhr) {
+			$.each(accounts, function(index, value) {
+				//création de la ligne tr
+				var ligne = $("<tr>").attr("data-account-id",value.id);
+				//création des colonnes
+				var pseudoCol = $("<td>");
+				var mailCol = $("<td>");
+				var roleCol = $("<td>");
+				var delCol = $("<td>");
+				//création et config des composants dans les colonnes
+				var pseudo = $("<label>").text(value.profile.pseudo);
+				var mail = $("<label>").text(value.mail);
+				var comboRole = $("<select>");
+				comboRole.on("change", value, updateEvent);
+				// recup la liste des roles et creer les options
+				var options = comboRole.prop('options');
+				$.each(roles, function(val, text) {
+					options[options.length] = new Option(text.role, text.id);
+				});
+				// selectionne la bonne option
+				comboRole.val(value.role.id);
+				// creer le bouton delete
+				var buttonDel = $( "<button>" ).text("Delete");
+				buttonDel.on("click",value.id,deleteEvent);
+				//ajoute les composants aux colonnes
+				pseudoCol.append(pseudo);
+				mailCol.append(mail);
+				roleCol.append(comboRole);
+				delCol.append(buttonDel);
+				//ajoute les colonnes à la ligne
+				ligne.append(pseudoCol);
+				ligne.append(mailCol);
+				ligne.append(roleCol);
+				ligne.append(delCol);
+				//ajoute la ligne au tableau
+				$("#accountsTable tbody").append(ligne);
 			});
-			// selectionne la bonne option
-			comboRole.val(value.role.id);
-			// creer le bouton delete
-			var buttonDel = $( "<button>" ).text("Delete");
-			buttonDel.on("click",value.id,deleteEvent);
-			//ajoute les composants aux colonnes
-			pseudoCol.append(pseudo);
-			mailCol.append(mail);
-			roleCol.append(comboRole);
-			delCol.append(buttonDel);
-			//ajoute les colonnes à la ligne
-			ligne.append(pseudoCol);
-			ligne.append(mailCol);
-			ligne.append(roleCol);
-			ligne.append(delCol);
-			//ajoute la ligne au tableau
-			$("#accountsTable tbody").append(ligne);
-		});
+		},
+		error : function(jqXHR, status, errorThrown) {
+			$("#comptes-alert").remove("success");
+			$("#comptes-alert").addClass("warning");
+			$(".comptes-alert-message").text("Une erreur technique s'est produite, prévenez l'administateur du site");
+			$("#comptes-alert").fadeIn(500);
+		},
+		dataType : "json"
 	});
 };
 
@@ -67,7 +84,7 @@ function updateEvent(event) {
 		url : "/rest/account",
 		contentType : "application/json; charset=utf-8",
 		data : JSON.stringify(value),
-		success : function(jqXHR, status, errorThrown) {
+		success : function(data, status, jqxhr) {
 			alert(status);
 		},
 		error : function(jqXHR, status, errorThrown) {
@@ -83,7 +100,7 @@ function deleteEvent(event){
 		type : "DELETE",
 		url : "/rest/account/"+id,
 		contentType : "application/json; charset=utf-8",
-		success : function(jqXHR, status, errorThrown) {
+		success : function(data, status, jqxhr) {
 			$("#accountsTable tr[data-account-id="+id+"]").remove();
 		},
 		error : function(jqXHR, status, errorThrown) {
@@ -91,3 +108,7 @@ function deleteEvent(event){
 		}
 	});
 };
+
+$(".close-comptes").on('click', function(event) {
+	$("#comptes-alert").fadeOut(500);
+});
