c9ad77c94788dddb830762f6f185d5de18cce8e4
mathieu-bellange <mbellange@gmail.com>
2014-04-04 18:09:13 +0200

    US7 : Création d'un compte - ajout des request AJAX check mail et pseudo

--- web-resources/src/main/js/loap-account.js
+++ web-resources/src/main/js/loap-account.js
@@ -14,7 +14,83 @@ function Profile(pseudo) {
 	this.pseudo = pseudo;
 }
 
+var pseudoIsValid = false;
+
+var mailIsValid = false;
+
+function checkPseudo(){
+	return pseudoIsValid;
+}
+
+
+/* DOM manipulation */
+
+function showPseudoIconValidation(valid){
+	pseudoIsValid = valid;
+	if (valid){
+		$("#pseudoPostFix").html("ok");
+	}else{
+		$("#pseudoPostFix").html("notOk");
+	}
+}
+
+function showPseudoIconLoading(){
+	$("#pseudoPostFix").html("load");
+}
+
+function showMailIconValidation(valid){
+	mailIsValid = valid;
+	if (valid){
+		$("#mailPostFix").html("ok");
+	}else{
+		$("#mailPostFix").html("notOk");
+	}
+}
+
+function showMailIconLoading(){
+	$("#mailPostFix").html("load");
+}
 /* AJAX */
+
+$("#pseudo").keyup( function(event){
+	showPseudoIconLoading();
+	var pseudo = $("#pseudo").val();
+	$.ajax({
+		type : "POST",
+		url : "/rest/signup_check/pseudo",
+		contentType : "application/json; charset=utf-8",
+		data : pseudo,
+		success : function(data, textStatus, jqXHR) {
+			showPseudoIconValidation(true);
+		},
+		error : function(jqXHR, status, errorThrown) {
+			if (jqXHR.status == 403){
+				showPseudoIconValidation(false);
+			}
+		},
+		dataType : "json"
+	});
+});
+
+$("#mail").keyup( function(event){
+	showMailIconLoading();
+	var mail = $("#mail").val();
+	$.ajax({
+		type : "POST",
+		url : "/rest/signup_check/mail",
+		contentType : "application/json; charset=utf-8",
+		data : mail,
+		success : function(data, textStatus, jqXHR) {
+			showMailIconValidation(true);
+		},
+		error : function(jqXHR, status, errorThrown) {
+			if (jqXHR.status == 403){
+				showMailIconValidation(false);
+			}
+		},
+		dataType : "json"
+	});
+});
 
 $("#bearAccount").submit(function(event){
 	if (document.getElementById("bearAccount").checkValidity()){
@@ -38,7 +114,9 @@ $("#bearAccount").submit(function(event){
 	}
 });
 
+
 /*Events */
+
 $("#compte-alert .close").on('click', function(event) {
 	$("#compte-alert").fadeOut(500);
 });
