536f61fc034e4207f1a75f061b3451cf5d28b53c
lny <lny.mrt@gmail.com>
2014-06-24 04:31:00 +0200

    US 2 : mise à jour de l'éditeur
    - JS : ajout d'un tag
    - JS : date prévisionnelle de publication
    - CSS : correctif visibilité

--- web-resources/src/main/resources/js/editor.js
+++ web-resources/src/main/resources/js/editor.js
@@ -1,4 +1,4 @@
-/* ------------------------------------------------------------------ */
+﻿/* ------------------------------------------------------------------ */
 /* # Templating */
 /* ------------------------------------------------------------------ */
 
@@ -71,94 +71,133 @@ $(document).ready(function(){
 /* # Events */
 /* ------------------------------------------------------------------ */
 
-/**
- * addTag()
- */
-function addTag(source, target) {
+/* Forecasted Date */
+function convert_date_format(digit, literal) {
+	var date = digit.val(); // Retrieve date value
+	var err_prefix = "<strong>Erreur</strong>&#8239;: " // Set error message string prefix
+	// var q = /^(\d{2})\/(\d{2})\/(\d{4})$/; // Set regular expression query DD/MM/YYYY
+	var q = /^(\d{2})(\/|-)(\d{2})(\/|-)(\d{4})$/; // Set regular expression query DD/MM/YYYY
+	var r = date.match(q); // Set regular expression result from match
+	if (r !== null) {
+		if (r[1] > 0 && r[1] <= 31 && r[3] > 0 && r[3] <= 12 && r[5] >= 2014) {
+			switch(r[3]) {
+				case "01" : r[3] = "janvier"; break;
+				case "02" : r[3] = "f&eacute;vrier"; break;
+				case "03" : r[3] = "mars"; break;
+				case "04" : r[3] = "avril"; break;
+				case "05" : r[3] = "mai"; break;
+				case "06" : r[3] = "juin"; break;
+				case "07" : r[3] = "juillet"; break;
+				case "08" : r[3] = "ao&ucirc;t"; break;
+				case "09" : r[3] = "septembre"; break;
+				case "10" : r[3] = "octobre"; break;
+				case "11" : r[3] = "novembre"; break;
+				case "12" : r[3] = "d&eacute;cembre"; break;
+			}
+			if (r[1] == "01" ) {
+				r[1] = "1<sup>er</sup>";
+			} else if (r[1].slice(0,1) == 0) {
+				r[1] = r[1].slice(1,2);
+			}
+			literal.html(r[1] + " " + r[3] + " " + r[5]);
+		} else if (r[1] == 0 || r[1] > 31) { // Day invalid
+			literal.html(err_prefix + "Jour invalide <small class='text-steel'>(<strong class='red'>01-31</strong>/MM/AAAA)</small>");
+		} else if (r[3] == 0 || r[3] > 12) { // Month invalid
+			literal.html(err_prefix + "Mois invalide <small class='text-steel'>(JJ/<strong class='red'>01-12</strong>/AAAA)</small>");
+		} else if (r[5] == 0 || r[5] < 2014) { // Year wrong
+			literal.html(err_prefix + "Ann&eacute;e incorrecte <small class='text-steel'>(JJ/MM/<strong class='red'>2014+</strong>)</small>");
+		} else { // Any other date mistake
+			literal.html(err_prefix + "Date erron&eacute;e &hellip;");
+		}
+	} else if (digit.val().length > 0) { // Wrong date format
+		literal.html(err_prefix + "Format de date incorrect <small class='text-steel'>(<strong class='red'>JJ/MM/AAAA</strong>)</small>");
+	} else {
+		literal.html("&hellip;");
+	}
+}
+$("html").on("blur", "#date", function() {
+	convert_date_format($(this), $("#date_literal"));
+});
+$("html").on("keydown", "#date", function(e) {
+	if (e.which == 13) { // Enter
+		convert_date_format($(this), $("#date_literal"));
+	}
+});
+
+/* Rubric and Category */
+$("html").on("change", "#rubric", function() {
+	$("#tag_rubric").text($(this).val());
+	$("#tag_rubric").removeClass();
+	if ($("#tag_rubric").parent("dd").hasClass("hide")) {
+		$("#tag_rubric").parent("dd").removeClass("hide")
+	}
+	if ($(this).children("option").filter(":selected").attr("data-color")) {
+		$("#tag_rubric").addClass("label radius " + $(this).children("option").filter(":selected").attr("data-color"));
+	} else {
+		$("#tag_rubric").addClass("label radius secondary");
+	}
+});
+$("html").on("change", "#category", function() {
+	$("#tag_category").text($(this).val());
+	if ($("#tag_category").parent("dd").hasClass("hide")) {
+		$("#tag_category").parent("dd").removeClass("hide")
+	}
+});
+
+/* Tags */
+var tag_num_lim = 8;
+var tag_err_msg = ["Limite de tags autoris&eacute;e atteinte", "Cette &eacute;tiquette a d&eacute;j&agrave; &eacute;t&eacute; choisie"];
+function add_tag(source, target) {
 	var str = $(source).val();
+	var is_valid = true;
 	if (str !== "") {
-		$(target).append("<dd><span class='label radius' data-alert>" + str + "<a href='javascript:void(0)' class='close'></a></span></dd>\n");
-		$(target).foundation("alert");
+		// Check if maxium of tags allowed is reached
+		if ($(target).children("dd").length >= tag_num_lim) {
+			$(source).next("small.error").html(tag_err_msg[0]);
+			$(source).next("small.error").removeClass("hide");
+			is_valid = false;
+		} else {
+			// Check if tag already exists
+			$(target).children("dd").each(function() {
+				if ($(this).children().text() == str) {
+					$(source).next("small.error").html(tag_err_msg[1]);
+					$(source).next("small.error").removeClass("hide");
+					is_valid = false;
+				}
+			});
+		}
+		// Add tag to tags list
+		if (is_valid == true) {
+			$(source).next("small.error").addClass("hide");
+			$(target).append("<dd data-alert><span class='label radius'>" + str + "<a href='javascript:void(0)' class='close'></a></span></dd>\n");
+			$(target).foundation("alert");
+			$(source).val("");
+		}
+	} else {
+		$(source).next("small.error").addClass("hide");
+	}
+}
+function hide_error(obj, clear) {
+	var clear = clear || false;
+	if (!$(obj).next("small.error").hasClass("hide")) {
+		$(obj).next("small.error").addClass("hide")
+		if (clear) {
+			$(obj).val("");
+		}
 	}
 }
 $("html").on("click", "#tag_add", function() {
-	addTag("#tag", "#tag_holder");
+	add_tag("#tag", "#tags");
 });
 $("html").on("keydown", "#tag", function(e) {
+	hide_error("#tag");
 	if (e.which == 13) { // Enter
-		addTag("#tag", "#tag_holder");
+		add_tag("#tag", "#tags");
 	}
 });
-
-
-/**
- * Selects
- * To be put in loap.js
- */
-// $("html").on("click", "body", function() {
-	// $(".select").each(function() {
-		// if ($(this).find(".options").css("display") != "none" && !$(this).is(":hover")) {
-			// $(this).find(".options").slideToggle("fast");
-		// }
-	// });
-// });
-$("html").on("blur", ".select", function(event) {
-	$(this).find(".options").slideUp("fast");
+$("html").on("focus", "#tag", function() {
+	hide_error("#tag", true);
 });
-$("html").on("click", ".select > .value", function() {
-	// alert($(this).next(".options").height());
-	// alert($(window).scrollTop());
-	// alert($(document).scrollTop());
-	// alert($(document).height());
-	// alert($(window).height());
-	$(this).next(".options").slideToggle("fast");
+$("html").on("blur", "#tag", function() {
+	hide_error("#tag");
 });
-$("html").on("click", ".select > .options > li", function() {
-	if (!$(this).hasClass("disabled")) {
-		var str = $(this).text();
-		$(this).addClass("selected");
-		$(this).siblings().removeClass("selected");
-		$(this).parent(".options").prev(".value").text(str);
-		$(this).parent(".options").slideToggle("fast");
-	}
-});
-$("html").on("keydown", ".select", function(event) {
-	if (event.which == 27) { // Escape
-		$(this).find(".options").slideUp("fast");
-	}
-	if (event.which == 13 || event.which == 32) { // Enter OR Space
-		var str = $(this).find(".options > li.selected").text();
-		$(this).find(".value").text(str);
-		$(this).find(".options").slideToggle("fast");
-	}
-	if (event.which == 38 || event.which == 40) { // Up or Down
-		event.preventDefault();
-		var idx = $(this).find(".options > li.selected").index();
-		if (idx == -1) {
-			$(this).find(".options > li").not(".disabled").first().addClass("selected");
-			idx = 0;
-		}
-		if (event.which == 38 && idx - 1 >= 0) { // Up
-			$(this).find(".options > li:eq(" + idx + ")").prevAll("li:not(.disabled)").first().addClass("selected");
-			if ($(this).find(".options > li:eq(" + idx + ")").prevAll("li:not(.disabled)").first().hasClass("selected")) {
-				$(this).find(".options > li:eq(" + idx + ")").removeClass("selected");
-			}
-		}
-		if (event.which == 40 && idx + 1 < $(this).find(".options > li").length) { // Down
-			$(this).find(".options > li:eq(" + idx + ")").nextAll("li:not(.disabled)").first().addClass("selected");
-			if ($(this).find(".options > li:eq(" + idx + ")").nextAll("li:not(.disabled)").first().hasClass("selected")) {
-				$(this).find(".options > li:eq(" + idx + ")").removeClass("selected");
-			}
-		}
-	}
-});
-$(document).ready(function() {
-	$(".select").each(function() {
-		if ($(this).find(".options > li").hasClass("selected")) {
-			var str = $(this).find(".options > li.selected").text();
-			$(this).find(".value").text(str);
-		}
-	});
-});
-// WARNING : add scroll bottom if window bottom position < box bottom position !!!
-// WARNING : add switch options while pressing down/up arrows if select focus and options not visible  !!!
