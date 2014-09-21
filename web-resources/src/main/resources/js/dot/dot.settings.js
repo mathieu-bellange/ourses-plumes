c91dd80a67f9741a4faa40c1be2e23337f376344
lny <lny.mrt@gmail.com>
2014-09-21 05:16:32 +0200

    Màj pages TEST
    - Ajout commentaires Templating
    - Màj Templating et Kitchen Sink
    - Suppression fichiers obsolètes

--- web-resources/src/main/resources/js/dot/dot.settings.js
+++ web-resources/src/main/resources/js/dot/dot.settings.js
@@ -5,7 +5,7 @@ doT.templateSettings = {
 	encode: regexp,        // replace illegal characters by html numeric entities
 	use: regexp,           // call partial template
 	define: regexp,        // set partial template
-	conditional: regexp,   // execute 'if-else if-else' structure
+	conditional: regexp,   // execute 'if, else if, else' structure
 	iterate: regexp,       // loop an array
 	varname: string,       // define the name of result function argument (i.e. the variable used for templating)
 	strip: boolean,        // erase whitespaces in result output (including tabulations and line breaks -- minify/compress)
