b5b4b79ecbbe5408350c6b7c819010dd802b4e8a
lny <lny.mrt@gmail.com>
2015-02-24 13:16:24 +0100

    Formatage HTML SQL FAQ
    - Encodage des entités HTML
    - N.B. : ce formatage devrait être opéré par la suite automatiquement
    lors de la validation des FAQ via la fonctionnalité d'édition.

--- server-resources/src/main/databases/META-INF/sql/insert_faq_dev.sql
+++ server-resources/src/main/databases/META-INF/sql/insert_faq_dev.sql
@@ -1,71 +1,71 @@
 Insert into freq_asked_question
   (id, question, answer,question_order)
 Values
-	(1, '<a href="http://fr.ulule.com/ourses-a-plumes/">Mais...Pourquoi « Les Ourses à plumes » ???</a>',
-	'<p>Nous n''avons pas d''explication réellement rationnelle à cette question. Ce que nous pouvons
-		vous dire, c''est que nous ne voulions pas d''un titre trop "intello", mais quelque chose allant 
-		vers l''imaginaire et qui sonne bien, que l''on retient. Nous avons donc choisi un animal : 
-		une ourse. Son côté "forte" nous plaisait. De là, est parti un brainstorming intense (si si !) 
-		pour choisir un nom. Vous avez donc échappé à notamment : "les grandes ourses", "ourses 
-		rousses", "les ourses tricotent", ou encore "le prisme des ourses". "Les Ourses à plumes" l''a 
-		emporté pour la double signification des "plumes", celles pour écrire, mais également celles 
-		qui servent à s''envoler. Un grain de fantaisie, mais aussi d''espoir, que le féminisme décolle 
-		dans notre société où il est toujours malheureusement aussi nécessaire.</p>',0),
-	(2,'Y a-t-il des critères pour écrire dans Les Ourses à plumes ?',
-	'<p>Nous avons fait le choix que nos rédactrices soient concernées au premier chef par les
-		oppressions qu''elles décrivent. De même qu’il nous paraît important que les articles traitant 
-		d’handicap ou du racisme soient écrits par des personnes en situation de handicap ou 
+	(1, 'Mais&hellip;Pourquoi &laquo;&#8201;Les Ourses &agrave; plumes&#8201;&raquo;&nbsp;???',
+	'<p>Nous n&rsquo;avons pas d&rsquo;explication r&eacute;ellement rationnelle &agrave; cette question. Ce que nous pouvons
+		vous dire, c&rsquo;est que nous ne voulions pas d&rsquo;un titre trop "intello", mais quelque chose allant 
+		vers l&rsquo;imaginaire et qui sonne bien, que l&rsquo;on retient. Nous avons donc choisi un animal&#8201;: 
+		une ourse. Son c&ocirc;t&eacute; "forte" nous plaisait. De l&agrave;, est parti un brainstorming intense (si si&nbsp;!) 
+		pour choisir un nom. Vous avez donc &eacute;chapp&eacute; &agrave; notamment&#8201;: "les grandes ourses", "ourses 
+		rousses", "les ourses tricotent", ou encore "le prisme des ourses". "Les Ourses &agrave; plumes" l&rsquo;a 
+		emport&eacute; pour la double signification des "plumes", celles pour &eacute;crire, mais &eacute;galement celles 
+		qui servent &agrave; s&rsquo;envoler. Un grain de fantaisie, mais aussi d&rsquo;espoir, que le f&eacute;minisme d&eacute;colle 
+		dans notre soci&eacute;t&eacute; o&ugrave; il est toujours malheureusement aussi n&eacute;cessaire.</p>',0),
+	(2,'Y a-t-il des crit&egrave;res pour &eacute;crire dans Les Ourses &agrave; plumes&nbsp;?',
+	'<p>Nous avons fait le choix que nos r&eacute;dactrices soient concern&eacute;es au premier chef par les
+		oppressions qu&rsquo;elles d&eacute;crivent. De m&ecirc;me qu&rsquo;il nous paraît important que les articles traitant 
+		d&rsquo;handicap ou du racisme soient &eacute;crits par des personnes en situation de handicap ou 
 		subissant le racisme, il nous semble primordial que cela soit les femmes (cis ou trans) et les 
-		hommes trans qui écrivent sur leur propre oppression.</p> 
-		<p>Nous sommes toutefois pas complètement fermées à l’idée d’avoir un jour des rédacteurs-
-		hommes cis féministes, mais cela restera des exceptions. Parce que nous considérons que 
-		les hommes cis ont déjà énormément d''espaces ailleurs pour s''exprimer, y compris sur le 
-		féminisme.</p>
-		<p>Nous parlons des Ourses, des rédactrices au féminin, même si des hommes trans, et 
-		exceptionnellement cis, peuvent y participer, car le masculin est déjà omniprésent et dominant 
-		dans le langage courant, alors même que les femmes sont majoritaires (dans la population...et 
-		surtout chez les Ourses !).</p>',1),
-	(3,'Que signifie le terme “intersectionnalité” ?',
-	'<p>C’est un terme forgé en 1991 par Kimberlé Crenshaw, une chercheuse féministe américaine
-		qui travaillait sur les violences subies par les femmes racisées des classes populaires. 
-		L’intersectionnalité, c’est celle des différentes oppressions (sexisme, racisme, homophobie, de 
-		classe, mais aussi handiphobie, transphobie...). Ce terme permet de souligner que chacune 
-		se trouve à l’intersection de plusieurs systèmes d’oppression, et qu’il faut tenir compte dans 
-		nos analyses de ces différentes oppressions. Ainsi, on ne peut pas analyser de la même 
-		façon la situation d’une femme racisée et celle d’une femme non-racisée, ou encore celle 
-		d’une femme hétéro et celle d’une femme lesbienne. Sans parler des femmes racisées et 
-		lesbiennes !</p>
-		<p>Le concept d’intersectionnalité permet d’éviter de nier les divergences de situations et donc 
-		de vécus, tout en les replaçant dans une perspective de systèmes et non de situations 
+		hommes trans qui &eacute;crivent sur leur propre oppression.</p> 
+		<p>Nous sommes toutefois pas compl&egrave;tement ferm&eacute;es &agrave; l&rsquo;id&eacute;e d&rsquo;avoir un jour des r&eacute;dacteurs-
+		hommes cis f&eacute;ministes, mais cela restera des exceptions. Parce que nous consid&eacute;rons que 
+		les hommes cis ont d&eacute;j&agrave; &eacute;norm&eacute;ment d&rsquo;espaces ailleurs pour s&rsquo;exprimer, y compris sur le 
+		f&eacute;minisme.</p>
+		<p>Nous parlons des Ourses, des r&eacute;dactrices au f&eacute;minin, m&ecirc;me si des hommes trans, et 
+		exceptionnellement cis, peuvent y participer, car le masculin est d&eacute;j&agrave; omnipr&eacute;sent et dominant 
+		dans le langage courant, alors m&ecirc;me que les femmes sont majoritaires (dans la population&hellip;et 
+		surtout chez les Ourses&nbsp;!).</p>',1),
+	(3,'Que signifie le terme “intersectionnalit&eacute;”&nbsp;?',
+	'<p>C&rsquo;est un terme forg&eacute; en 1991 par Kimberl&eacute; Crenshaw, une chercheuse f&eacute;ministe am&eacute;ricaine
+		qui travaillait sur les violences subies par les femmes racis&eacute;es des classes populaires. 
+		L&rsquo;intersectionnalit&eacute;, c&rsquo;est celle des diff&eacute;rentes oppressions (sexisme, racisme, homophobie, de 
+		classe, mais aussi handiphobie, transphobie&hellip;). Ce terme permet de souligner que chacune 
+		se trouve &agrave; l&rsquo;intersection de plusieurs syst&egrave;mes d&rsquo;oppression, et qu&rsquo;il faut tenir compte dans 
+		nos analyses de ces diff&eacute;rentes oppressions. Ainsi, on ne peut pas analyser de la m&ecirc;me 
+		façon la situation d&rsquo;une femme racis&eacute;e et celle d&rsquo;une femme non-racis&eacute;e, ou encore celle 
+		d&rsquo;une femme h&eacute;t&eacute;ro et celle d&rsquo;une femme lesbienne. Sans parler des femmes racis&eacute;es et 
+		lesbiennes&nbsp;!</p>
+		<p>Le concept d&rsquo;intersectionnalit&eacute; permet d&rsquo;&eacute;viter de nier les divergences de situations et donc 
+		de v&eacute;cus, tout en les replaçant dans une perspective de syst&egrave;mes et non de situations 
 		individuelles.</p>',2),
-	(4,'Cette revue soutient-elle les luttes des trans, des travailleuses-rs du sexe et des femmes voilées ?',
-		'<p>L''objectif de mettre en place un webzine qui serait un outil pour l’essor du féminisme, en
-		rapportant ce qui se passe sur tous les fronts de ce dernier. Notre ligne éditoriale est claire : 
-		pour nous, le féminisme est l’affaire de toutes les femmes – et des hommes trans. Nous 
-		défendons donc un féminisme inclusif, ce qui signifie que nous ne véhiculerons pas de 
-		discours excluant vis-à-vis de telle ou telle catégorie de femmes, et qu’au contraire nous 
+	(4,'Cette revue soutient-elle les luttes des trans, des travailleuses-rs du sexe et des femmes voil&eacute;es&nbsp;?',
+		'<p>L&rsquo;objectif de mettre en place un webzine qui serait un outil pour l&rsquo;essor du f&eacute;minisme, en
+		rapportant ce qui se passe sur tous les fronts de ce dernier. Notre ligne &eacute;ditoriale est claire&#8201;: 
+		pour nous, le f&eacute;minisme est l&rsquo;affaire de toutes les femmes – et des hommes trans. Nous 
+		d&eacute;fendons donc un f&eacute;minisme inclusif, ce qui signifie que nous ne v&eacute;hiculerons pas de 
+		discours excluant vis-&agrave;-vis de telle ou telle cat&eacute;gorie de femmes, et qu&rsquo;au contraire nous 
 		voulons ouvrir un espace de parole pour toutes.</p>
-		<p>Nous soutenons l’autoorganisation et les luttes de toutes les femmes sur une base 
-		antipatriarcale et anticapitaliste (donc non, on ne soutient pas C. Boutin !).</p>',3),
-	(5,'Comment peut-on soutenir votre site ? Peut-on y participer ?',
-		'<p>Pour nous soutenir, il y a tout d’abord des gestes gratuits, qui peuvent avoir beaucoup
-		d’impact : parler de nous autour de vous, relayez nos articles sur les réseaux sociaux...Nous 
-		sommes aussi ouvertes à vos suggestions de sujets ou d’événements à rajouter dans notre 
+		<p>Nous soutenons l&rsquo;autoorganisation et les luttes de toutes les femmes sur une base 
+		antipatriarcale et anticapitaliste (donc non, on ne soutient pas C. Boutin&nbsp;!).</p>',3),
+	(5,'Comment peut-on soutenir votre site&nbsp;? Peut-on y participer&nbsp;?',
+		'<p>Pour nous soutenir, il y a tout d&rsquo;abord des gestes gratuits, qui peuvent avoir beaucoup
+		d&rsquo;impact&#8201;: parler de nous autour de vous, relayez nos articles sur les r&eacute;seaux sociaux&hellip;Nous 
+		sommes aussi ouvertes &agrave; vos suggestions de sujets ou d&rsquo;&eacute;v&eacute;nements &agrave; rajouter dans notre 
 		agenda.</p>
-		<p><strong>Nous cherchons encore des rédactrices et des illustratrices (dessinatrices, 
-		photographes), bénévoles,</strong> qui soient en accord avec notre ligne éditoriale, n’hésitez donc 
-		pas à nous contacter si vous êtes intéressée par le projet.</p>
-		<p>Financièrement, nous sommes toujours preneuses de dons, car la campagne de crowfunding 
-		sur Ulule ne permettra que de financer un an d’hébergement. Il nous reste également d’autres 
-		frais de fonctionnement à prendre en compte, ainsi que peut-être un investissement dans du 
-		matériel professionnel. Pour assurer la pérennité du site, un bouton Paypal devrait être bientôt 
-		mis en place, en attendant vous pouvez aussi nous envoyer un chèque...</p>',4),
-	(6,'Comment le site est-il financé ? Exploitez-vous vos rédactrices ?',
-		'<p>Le lancement du site a été financé par une campagne de crowdfunding. Nous mettrons en
-		place dans l''année des propositions de financement par dons ou par adhésion à l''association 
-		de gestion du site pour les personnes qui souhaitent s''investir davantage.</p>
-		<p>Toutes les rédactrices sont bénévoles, tout comme l''équipe de développement du site. 
-		Aucune personne n''est ni n''a été rémunérée pour le webzine, que ce soit sur le plan technique 
-		ou rédactionnel. Le webzine est géré par une association à but non lucratif.</p>
-		<p>Ce modèle nous permet de garantir l''indépendance éditoriale du webzine et de garantir un site 
-		sans publicité. Les Ourses à plumes ne font pas de bénéfices et n''ont pas vocation à en faire !</p>',5);
+		<p><strong>Nous cherchons encore des r&eacute;dactrices et des illustratrices (dessinatrices, 
+		photographes), b&eacute;n&eacute;voles,</strong> qui soient en accord avec notre ligne &eacute;ditoriale, n&rsquo;h&eacute;sitez donc 
+		pas &agrave; nous contacter si vous &ecirc;tes int&eacute;ress&eacute;e par le projet.</p>
+		<p>Financi&egrave;rement, nous sommes toujours preneuses de dons, car la campagne de crowfunding 
+		sur Ulule ne permettra que de financer un an d&rsquo;h&eacute;bergement. Il nous reste &eacute;galement d&rsquo;autres 
+		frais de fonctionnement &agrave; prendre en compte, ainsi que peut-&ecirc;tre un investissement dans du 
+		mat&eacute;riel professionnel. Pour assurer la p&eacute;rennit&eacute; du site, un bouton Paypal devrait &ecirc;tre bient&ocirc;t 
+		mis en place, en attendant vous pouvez aussi nous envoyer un ch&egrave;que&hellip;</p>',4),
+	(6,'Comment le site est-il financ&eacute;&nbsp;? Exploitez-vous vos r&eacute;dactrices&nbsp;?',
+		'<p>Le lancement du site a &eacute;t&eacute; financ&eacute; par une <a href="http://fr.ulule.com/ourses-a-plumes/">campagne de crowdfunding</a>. Nous mettrons en
+		place dans l&rsquo;ann&eacute;e des propositions de financement par dons ou par adh&eacute;sion &agrave; l&rsquo;association 
+		de gestion du site pour les personnes qui souhaitent s&rsquo;investir davantage.</p>
+		<p>Toutes les r&eacute;dactrices sont b&eacute;n&eacute;voles, tout comme l&rsquo;&eacute;quipe de d&eacute;veloppement du site. 
+		Aucune personne n&rsquo;est ni n&rsquo;a &eacute;t&eacute; r&eacute;mun&eacute;r&eacute;e pour le webzine, que ce soit sur le plan technique 
+		ou r&eacute;dactionnel. Le webzine est g&eacute;r&eacute; par une association &agrave; but non lucratif.</p>
+		<p>Ce mod&egrave;le nous permet de garantir l&rsquo;ind&eacute;pendance &eacute;ditoriale du webzine et de garantir un site 
+		sans publicit&eacute;. Les Ourses &agrave; plumes ne font pas de b&eacute;n&eacute;fices et n&rsquo;ont pas vocation &agrave; en faire&nbsp;!</p>',5);
