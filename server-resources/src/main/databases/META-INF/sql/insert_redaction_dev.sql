236563130e1538b982269dc8066f9e479594128c
mathieu-bellange <mbellange@mail.com>
2015-03-26 16:04:06 +0100

    ajout nouvelles catégories

--- server-resources/src/main/databases/META-INF/sql/insert_redaction_dev.sql
+++ server-resources/src/main/databases/META-INF/sql/insert_redaction_dev.sql
@@ -1,14 +1,21 @@
 Insert into category
   (id, category)
 Values
-  (1, 'Brèves'),
-  (2, 'Reportage'),
-  (3, 'Spotlights'),
-  (4, 'Interview'),
-  (5, 'Tribune'),
-  (6, 'Revue du web'),
-  (7, 'Retour de terrain'),
-  (8, 'Forme libre');
+  (1,  'Brève'),
+  (2,  'Reportage'),
+  (3,  'Spotlights'),
+  (4,  'Interview'),
+  (5,  'Tribune'),
+  (6,  'Revue du web'),
+  (7,  'Retour de terrain'),
+  (8,  'Forme libre'),
+  (9,  'Chronique'),
+  (10, 'Portrait'),
+  (11, 'Article d''analyse'),
+  (12, 'Critique'),
+  (13, 'Billet d''humeur'),
+  (14, 'Diaporama'),
+  (15, 'Dessin');
 
 Insert into rubrique
   (id, rubrique, classe, path)
